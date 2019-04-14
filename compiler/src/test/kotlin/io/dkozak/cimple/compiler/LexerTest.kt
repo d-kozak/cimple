5a49366f5a9a039697e51ffe8c9c088ab595c252
dkozak <dkozak94@gmail.com>
2019-04-14 23:15:26 +0100

    cleanup the test code

--- compiler/src/test/kotlin/io/dkozak/cimple/compiler/LexerTest.kt
+++ compiler/src/test/kotlin/io/dkozak/cimple/compiler/LexerTest.kt
@@ -1,10 +1,21 @@
 package io.dkozak.cimple.compiler
 
 
+import org.assertj.core.api.AbstractObjectAssert
 import org.assertj.core.api.Assertions.assertThat
-
+import org.assertj.core.api.ObjectAssert
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+
+
+fun ObjectAssert<*>.isIntToken(expectedValue: Int): AbstractObjectAssert<*, *> = this.isInstanceOf(IntToken::class.java)
+        .extracting { token -> (token as IntToken).value }
+        .isEqualTo(expectedValue)
+
+
+fun ObjectAssert<*>.isDoubleToken(expectedValue: Double): AbstractObjectAssert<*, *> = this.isInstanceOf(DoubleToken::class.java)
+        .extracting { token -> (token as DoubleToken).value }
+        .isEqualTo(expectedValue)
 
 
 class LexerTest {
@@ -13,65 +24,51 @@ class LexerTest {
     inner class Numbers {
         @Test
         fun `Single integer 42`() {
-            val input = "42"
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is IntToken)
-            assertThat((token as IntToken).value).isEqualTo(42)
+            val lexer = Lexer("42")
+            assertThat(lexer.getNextToken())
+                    .isIntToken(42)
         }
 
         @Test
         fun `Single integer 1`() {
-            val input = "1"
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is IntToken) { "Should be Int" }
-            assertThat((token as IntToken).value).isEqualTo(1)
+            val lexer = Lexer("1")
+            assertThat(lexer.getNextToken())
+                    .isIntToken(1)
         }
 
         @Test
         fun `Single integer 100`() {
-            val input = "100"
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is IntToken) { "Should be Int" }
-            assertThat((token as IntToken).value).isEqualTo(100)
+            val lexer = Lexer("100")
+            assertThat(lexer.getNextToken())
+                    .isIntToken(100)
         }
 
         @Test
         fun `Single double 4,2`() {
-            val input = "4.2"
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is DoubleToken) { "Should be Double" }
-            assertThat((token as DoubleToken).value).isEqualTo(4.2)
+            val lexer = Lexer("4.2")
+            assertThat(lexer.getNextToken())
+                    .isDoubleToken(4.2)
         }
 
         @Test
         fun `Single double 135,666`() {
-            val input = "135.666"
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is DoubleToken) { "Should be Double" }
-            assertThat((token as DoubleToken).value).isEqualTo(135.666)
+            val lexer = Lexer("135.666")
+            assertThat(lexer.getNextToken())
+                    .isDoubleToken(135.666)
         }
 
         @Test
         fun `Single integer 135 with something behing it`() {
-            val input = "135."
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is IntToken) { "Should be Double" }
-            assertThat((token as IntToken).value).isEqualTo(135)
+            val lexer = Lexer("135.")
+            assertThat(lexer.getNextToken())
+                    .isIntToken(135)
         }
 
         @Test
-        fun `Lex a double with something behing it`() {
-            val input = "130.g."
-            val lexer = Lexer(input)
-            val token = lexer.getNextToken()
-            assert(token is IntToken) { "Should be Double" }
-            assertThat((token as IntToken).value).isEqualTo(130)
+        fun `Lex a single 130 with something behing it`() {
+            val lexer = Lexer("130.g.")
+            assertThat(lexer.getNextToken())
+                    .isIntToken(130)
         }
 
     }
@@ -81,12 +78,12 @@ class LexerTest {
         @Test
         fun `Plus minus multiply divide`() {
 
-            val input = "+-*/"
+            val lexer = Lexer("+-*/")
             val expectedTokens = listOf(Plus, Minus, Multiply, Divide)
-            val lexer = Lexer(input)
 
             for (expectedToken in expectedTokens) {
-                assertThat(lexer.getNextToken()).isEqualTo(expectedToken)
+                assertThat(lexer.getNextToken())
+                        .isEqualTo(expectedToken)
             }
 
         }
@@ -97,50 +94,35 @@ class LexerTest {
 
         @Test
         fun `1 + 1`() {
-            val input = "1+1"
-            val lexer = Lexer(input)
+            val lexer = Lexer("1+1")
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(1)
+                    .isIntToken(1)
             assertThat(lexer.getNextToken()).isEqualTo(Plus)
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(1)
+                    .isIntToken(1)
 
         }
 
         @Test
         fun `123 + 36`() {
-            val input = "123+36"
-            val lexer = Lexer(input)
+            val lexer = Lexer("123+36")
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(123)
+                    .isIntToken(123)
             assertThat(lexer.getNextToken()).isEqualTo(Plus)
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(36)
+                    .isIntToken(36)
 
         }
 
 
         @Test
         fun `123,44 + 36,64`() {
-            val input = "123.44+36.64"
-            val lexer = Lexer(input)
+            val lexer = Lexer("123.44+36.64")
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(DoubleToken::class.java)
-                    .extracting { token -> (token as DoubleToken).value }
-                    .isEqualTo(123.44)
+                    .isDoubleToken(123.44)
             assertThat(lexer.getNextToken()).isEqualTo(Plus)
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(DoubleToken::class.java)
-                    .extracting { token -> (token as DoubleToken).value }
-                    .isEqualTo(36.64)
+                    .isDoubleToken(36.64)
 
         }
 
@@ -151,38 +133,26 @@ class LexerTest {
 
         @Test
         fun `420 + 8080,42`() {
-            val input = "420 + 8080.42"
-            val lexer = Lexer(input)
+            val lexer = Lexer("420 + 8080.42")
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(420)
+                    .isIntToken(420)
             assertThat(lexer.getNextToken()).isEqualTo(Plus)
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(DoubleToken::class.java)
-                    .extracting { token -> (token as DoubleToken).value }
-                    .isEqualTo(8080.42)
+                    .isDoubleToken(8080.42)
 
         }
 
         @Test
         fun `420   +        8080,42    divide   135`() {
-            val input = "420   +        8080.42    /   135"
-            val lexer = Lexer(input)
+            val lexer = Lexer("420   +        8080.42    /   135")
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(420)
+                    .isIntToken(420)
             assertThat(lexer.getNextToken()).isEqualTo(Plus)
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(DoubleToken::class.java)
-                    .extracting { token -> (token as DoubleToken).value }
-                    .isEqualTo(8080.42)
+                    .isDoubleToken(8080.42)
             assertThat(lexer.getNextToken()).isEqualTo(Divide)
             assertThat(lexer.getNextToken())
-                    .isInstanceOf(IntToken::class.java)
-                    .extracting { token -> (token as IntToken).value }
-                    .isEqualTo(135)
+                    .isIntToken(135)
 
         }
 
