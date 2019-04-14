f1994897e8504e80fa77c62c5de92d4db1ac059a
dkozak <dkozak94@gmail.com>
2019-04-14 14:39:22 +0100

    init backend

---
+++ backend/webserver/src/test/kotlin/io/dkozak/cimple/webserver/WebserverApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.dkozak.cimple.webserver
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WebserverApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
