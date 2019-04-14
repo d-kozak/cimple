76d3ff3afe0aebf510768a4ad1c524117186e7a9
dkozak <dkozak94@gmail.com>
2019-04-14 14:55:22 +0100

    add npm command to build the frontend and copy it to the webserver static folder

---
+++ backend/webserver/src/main/resources/static/precache-manifest.e6e255b5981064d2cf85dac7451a40cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+    {
+        "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+        "url": "/static/media/logo.5d5d9eef.svg"
+    },
+    {
+        "revision": "42ac5946195a7306e2a5",
+        "url": "/static/js/runtime~main.a8a9905a.js"
+    },
+    {
+        "revision": "15a3448f621650717363",
+        "url": "/static/js/main.68ce1318.chunk.js"
+    },
+    {
+        "revision": "45124f853ba07aa965c0",
+        "url": "/static/js/2.6efc73d3.chunk.js"
+    },
+    {
+        "revision": "15a3448f621650717363",
+        "url": "/static/css/main.85f6aebc.chunk.css"
+    },
+    {
+        "revision": "d72126f93fd5075f58a17af2ab893f43",
+        "url": "/index.html"
+    }
+];
